9cf712033b36bd625aa95c377a15822872e001b1
ambiversive <ambiversive@fancey.ca>
2011-10-29 23:39:01 -0400

    submitlink

--- links/submit_link.php
+++ links/submit_link.php
@@ -1,17 +1,21 @@
 <?php
 
     include("../top.php");
-    $uid = $_SESSION['session_userid'];
-    $tit = $_POST['title'];
-    $url = $_POST['url'];
-    if(strpos($url,"?")===false){
 
-    }else{
-        $url = substr($url, 0, strpos($url,"?"));
+    if($access == 0){
+
+        $uid = $_SESSION['session_userid'];
+        $tit = $_POST['title'];
+        $url = $_POST['url'];
+        if(strpos($url,"?")===false){
+
+        }else{
+            $url = substr($url, 0, strpos($url,"?"));
+        }
+        $url_ = substr($url, 7);
+
+        Link::newLink($url, $tit, $uid);
+        $cmd = "wget -E -H -k -K -p $url -P ../cache/ -o ../cache/log -e robots=off";
+        exec("bash -c \"exec nohup setsid $cmd > /dev/null 2>&1 &\""); 
+        chatline("submits a link <a href=\"$url\">$tit</a><a href=\"cache/$url_\">&nbsp;[cached]</a>.");
     }
-    $url_ = substr($url, 7);
-
-    Link::newLink($url, $tit, $uid);
-    $cmd = "wget -E -H -k -K -p $url -P ../cache/ -o ../cache/log -e robots=off";
-    exec("bash -c \"exec nohup setsid $cmd > /dev/null 2>&1 &\""); 
-    chatline("submits a link <a href=\"$url\">$tit</a><a href=\"cache/$url_\">&nbsp;[cached]</a>.");
