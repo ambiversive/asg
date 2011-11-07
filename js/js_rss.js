b322e7f1f1f00e725d7ce5629c813b205e181274
ambiversive <ambiversive@fancey.ca>
2011-11-07 15:11:37 -0500

    cleaned up rss style

--- js/js_rss.js
+++ js/js_rss.js
@@ -16,14 +16,16 @@ var asgRss = {
         whereTo.html('');
         $.scrollTo($('#rss'), 800);
         $('#rss_full').scrollTo(whereTo, 800);
+        whereTo.append('<table>');
         for(link in response){
             sub = response[link].subreddit;
             title_en = response[link].title_en;
             title = response[link].title;
             title_ = asgRss.addslashes(title);
             url = response[link].url;
-            whereTo.append('<p><a target="_blank" href="http://reddit.com/r/'+sub+'/submit?url='+url+'&title='+title_en+'" title="submit to reddit/r/'+sub+'">&#9664;</a><a target="_blank" href="'+url+'">&nbsp;'+title+'&nbsp;</a><a onclick="asgConfig.simcom(\'/bload '+url+'\')" title="load in metabrowser">&#9724;</a><a title="save locally" onclick="$(this).hide();asgConfig.submitLink(\''+url+'\',\''+title_+'\');">&#9654;</a></p>');
+            whereTo.append('<tr><td class=\"rss_reddit\"><a target="_blank" href="http://reddit.com/r/'+sub+'/submit?url='+url+'&title='+title_en+'" title="submit to reddit/r/'+sub+'">&#9664;</a></td><td class=\"rss_link\"><a target="_blank" href="'+url+'">&nbsp;'+title+'&nbsp;</a></td><td class=\"rss_meta\"><a onclick="asgConfig.simcom(\'/bload '+url+'\')" title="load in metabrowser">&#9724;</a></td><td class=\"rss_save\"><a title="save locally" onclick="$(this).hide();asgConfig.submitLink(\''+url+'\',\''+title_+'\');">&#9654;</a></td></tr>');
         }
+        whereTo.append('</table>');
     },
 
     addslashes: function(str) {
