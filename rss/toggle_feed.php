98ace009a3405513a41a96002a70fe80d072e7aa
ambiversive <ambiversive@fancey.ca>
2011-11-08 10:04:48 -0330

    added feed filters

---
+++ rss/toggle_feed.php
@@ -0,0 +1,19 @@
+<?php
+    include("../top.php");
+
+    $subs = $_SESSION['subs'];
+    $toggle = $_POST['toggle'];
+    
+    if(isset($toggle)){
+        if(!in_array($toggle,$subs)){
+            $subs[] = $toggle;   
+            $_SESSION['subs'] = $subs;
+        }else{
+            $subs = array_diff($subs, array("$toggle"));
+            $_SESSION['subs'] = $subs;
+            
+        }
+        
+    }
+    chatline("adds $toggle to subs.");
+?>
